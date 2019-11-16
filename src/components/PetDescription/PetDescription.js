55a9c4c1451f1be8fe4e0d9902eba62dfd7266c6
Quasar Wei <quasar.wei@outlook.com>
2019-11-15 18:23:11 -0800

    pet descriptions

--- src/components/PetDescription/PetDescription.js
+++ src/components/PetDescription/PetDescription.js
@@ -1,39 +1,49 @@
 import React from 'react';
+import './PetDescription.css';
 
 export default class PetDescription extends React.Component {
-
   state = {
-    pets: this.props.pets,
-    currentpet: this.props.pets
-  }
+    currentPetIndex: 0
+  };
+
+  goToNextPet = () => {
+    let petIndex = this.state.currentPetIndex;
+    if (petIndex < this.props.pets.length - 1) {
+      this.setState({
+        currentPetIndex: ++petIndex
+      });
+    }
+  };
+
+  goToPreviousPet = () => {
+    let petIndex = this.state.currentPetIndex;
+    if (petIndex > 0) {
+      this.setState({
+        currentPetIndex: --petIndex
+      });
+    }
+  };
 
   render() {
-
     return (
       <>
-      <button className='next-button'>next</button>
-        <img alt=''>
-        </img>
+        <button className='prev-button' onClick={() => this.goToPreviousPet()}>prev</button>
+        <button className='next-button' onClick={() => this.goToNextPet()}>next</button>
+        <img
+          className='pet-image'
+          src={this.props.pets[this.state.currentPetIndex].imageURL}
+          alt={this.props.pets[this.state.currentPetIndex].imageDescription}
+        />
         <div className='pet-details'>
           <ul>
-            <li>
-              Name: 'sdfsdf'
-            </li>
-            <li>
-              Sex: 'sdfsdf'
-            </li>
-            <li>
-              Age: 'sdfsdf'
-            </li>
-            <li>
-              Breed: 'sdfsdf'
-            </li>
-            <li>
-              Story: 'sdfsdf'
-            </li>
+            <li>Name: {this.props.pets[this.state.currentPetIndex].name}</li>
+            <li>Sex: {this.props.pets[this.state.currentPetIndex].sex}</li>
+            <li>Age: {this.props.pets[this.state.currentPetIndex].age}</li>
+            <li>Breed: {this.props.pets[this.state.currentPetIndex].breed}</li>
+            <li>Story: {this.props.pets[this.state.currentPetIndex].story}</li>
           </ul>
         </div>
       </>
     );
-  };
-};
+  }
+}
