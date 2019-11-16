1051b0730137608734b38cf9d4e18f7e11984e57
Marlon Agno <mdj.agno@gmail.com>
2019-11-15 22:25:38 -0500

    added adopted gallery

--- src/components/Pets/Pets.js
+++ src/components/Pets/Pets.js
@@ -6,22 +6,36 @@ import PetDescription from '../PetDescription/PetDescription';
 
 export default class Pets extends React.Component {
   state = {
-    adoptedCats: [],
-    adoptedDogs: []
+    adoptedPets: []
   }
   
   componentDidMount() {
     this.setState({
-      adoptedCats: this.props.adoptedCats,
-      adoptedDogs: this.props.adoptedDogs
+      adoptedPets: [...this.props.adoptedDogs, ...this.props.adoptedCats]
     })
+  }
+
+  renderAdoptedPets() {
+    let adoptedPets = this.state.adoptedPets.map((pet, index) => {
+      return(
+        <li key={index} className='adoptedPet'>
+          <img src={pet.imageURL}/>
+        </li>
+      )
+    })
+    return adoptedPets;
   }
   render() {
     console.log(this.props)
+    console.log(this.state)
     return(
       <div className='pets'>
         <PetDescription className='cat-desc' pets={this.props.cats} ></PetDescription>
         <PetDescription className='dogs-desc' pets={this.props.dogs}></PetDescription>
+        <ul className='adoptedPets'>
+          {this.renderAdoptedPets()}
+          <h3>Test</h3>
+        </ul>
 
       </div>
     )
